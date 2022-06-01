68cdd6b68843530a3c5b9c580a3e6b3c19397bd8
Eagle Zhen <eagle.zihao@gmail.com>
2022-06-01 18:32:15 +0800

    added random + different style for selected label

--- Python/Flask/application.py
+++ Python/Flask/application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from random import randrange
 
 app = Flask(__name__,template_folder="./")
 
@@ -7,11 +8,16 @@ def yo():
 	# 第一个是pass进去的variable，?name=
 	# name = request.args.get("name", "world")
 
+	total_no_of_students = 6*35
+	lucky_dog = randrange(1,total_no_of_students)
+
 	name_list=""
 	for class_no in range (65,65+6):
 		for id in range (1,35):
-			name_list += f"<label id=\"rounded_box_green\">6{chr(class_no)}&nbsp{str(id).zfill(2)}</label>\n"
-		name_list += "<br><br><br>"
+			if ((class_no-65)*35+id==lucky_dog):
+				name_list += f"<label id=\"rounded_box_green_selected\">6{chr(class_no)}&nbsp{str(id).zfill(2)}</label>\n"
+			else:
+				name_list += f"<label id=\"rounded_box_green\">6{chr(class_no)}&nbsp{str(id).zfill(2)}</label>\n"
 
 	return render_template("index.html", name_box=name_list)
 
